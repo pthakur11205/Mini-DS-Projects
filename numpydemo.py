62a249cfb9407eb1bad37e74a70aa5bcbf5b1707
pthakur11205 <pthakur@uw.edu>
2024-07-03 15:02:01 -0400

    more changes

--- numpydemo.py
+++ numpydemo.py
@@ -5,6 +5,12 @@ b = np.array([1.2, 3.5, 5.1])
 print(a.dtype)
 print(b.dtype)
 
-a = np.arange(15).reshape(5, 3)
+# a = np.arange(15).reshape(5, 3)
 
-print(a)
+# print(a)
+
+#Basic arithmetic
+c = a - b
+print(c)
+c = a * b
+print(c)
