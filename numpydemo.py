59f672666c4cd0901cb631b6d75e5cc86eadb330
pthakur11205 <pthakur@uw.edu>
2024-07-03 15:58:44 -0400

    small edits

--- numpydemo.py
+++ numpydemo.py
@@ -1,4 +1,6 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 a = np.array([2, 3, 4])
 b = np.array([1.2, 3.5, 5.1])
@@ -23,4 +25,25 @@ print(a[2:5])
 print(a[:4])
 print(a[-6:])
 
+a = np.arange(12).reshape(3, 4)
+print(a)
 
+#splitting a into 2
+b = np.hsplit(a, 2)
+print(b)
+
+
+#check dimensions
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim) #0
+print(b.ndim) #1
+print(c.ndim) #2
+print(d.ndim) #3
+
+sns.distplot([0, 1, 2, 3, 4, 5])
+
+plt.show()
