beaf27eaa779994124f366dc28196bba492ca74f
pthakur11205 <pthakur@uw.edu>
2024-07-05 20:57:28 -0400

    small

--- csvtesting.py
+++ csvtesting.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data.csv', index_col = 0)
 
+print(df)
+print(df.index)
+# print(df.columns[0])
+
+#editing labels with numpy methods
+df.index = np.arange(1, 8)
 print(df)
