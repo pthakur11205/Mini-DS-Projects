a60881efea9d2d4682241658a697cc3b7586958f
pthakur11205 <pthakur@uw.edu>
2024-07-11 16:10:22 -0400

    adding labels

--- maplolib.py
+++ maplolib.py
@@ -5,3 +5,11 @@ dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
 dev_y = [38496, 42000, 46752, 49320, 53200,
          56000, 62316, 64928, 67317, 68748, 73752]
+
+plt.plot(dev_x, dev_y)
+
+
+plt.xlabel("ages")
+plt.ylabel('median salary')
+plt.title('Median Salary (USD) by Age')
+plt.show()
