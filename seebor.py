6cb8df4b57709b90c0dd083de97fc7aec434caa8
pthakur11205 <pthakur@uw.edu>
2024-07-13 20:36:07 -0400

    plots

--- seebor.py
+++ seebor.py
@@ -1,6 +1,20 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 #crash dataset from seaborn built-in data
 crash_df = sns.load_dataset('car_crashes')
 
-print(crash_df)
+#distribution plots
+sns.displot(crash_df['not_distracted'], kde=False)
+
+#joint plots
+sns.jointplot(x='speeding', y='alcohol', data = crash_df, kind = 'hex')
+
+#kde plot
+sns.kdeplot(crash_df['alcohol'])
+
+#pair plot
+sns.pairplot(crash_df)
+plt.show()
