3685d38ec75a3fb1d988a424390e37d421f2e534
pthakur11205 <pthakur@uw.edu>
2024-07-22 14:29:08 -0400

    studying descriptive statistics

---
+++ descStats.py
@@ -0,0 +1,17 @@
+import math
+import statistics
+import numpy as np
+import scipy.stats
+import pandas as pd
+
+x = [8.0, 1, 2.5, 4, 28.0]
+x_with_nan = [8.0, 1, 2.5, math.nan, 4, 28.0]
+print(x)
+print(x_with_nan)
+
+y, y_with_nan = np.array(x), np.array(x_with_nan)
+z, z_with_nan = pd.Series(x), pd.Series(x_with_nan)
+print(y)
+print(y_with_nan)
+print(z)
+print(z_with_nan)
