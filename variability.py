b80c14091504e48242a4c35ea84aec96e6df3d75
pthakur11205 <pthakur@uw.edu>
2024-07-22 15:13:47 -0400

    variability

--- variability.py
+++ variability.py
@@ -6,3 +6,12 @@ import pandas as pd
 
 x = [8.0, 1, 2.5, 4, 28.0]
 x_with_nan = [8.0, 1, 2.5, math.nan, 4, 28.0]
+
+#mean
+print(statistics.mean(x))
+
+#sample variance
+print(statistics.variance(x))
+
+#std
+print(statistics.stdev(x))
