2c97f53bea0ee28b00c00195f9c77e2d2bc56474
pthakur11205 <pthakur@uw.edu>
2024-07-22 14:31:39 -0400

    intro to mean

--- descStats.py
+++ descStats.py
@@ -6,12 +6,19 @@ import pandas as pd
 
 x = [8.0, 1, 2.5, 4, 28.0]
 x_with_nan = [8.0, 1, 2.5, math.nan, 4, 28.0]
-print(x)
-print(x_with_nan)
+# print(x)
+# print(x_with_nan)
 
 y, y_with_nan = np.array(x), np.array(x_with_nan)
 z, z_with_nan = pd.Series(x), pd.Series(x_with_nan)
-print(y)
-print(y_with_nan)
-print(z)
-print(z_with_nan)
+# print(y)
+# print(y_with_nan)
+# print(z)
+# print(z_with_nan)
+
+mean = statistics.mean(x)
+print(mean)
+
+#nan values return a NaN mean
+mean = statistics.mean(x_with_nan)
+print(mean)
