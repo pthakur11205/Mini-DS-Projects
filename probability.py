f22251fe096cd0d3d9398c7dd30f8c824de9c3c7
pthakur11205 <pthakur@uw.edu>
2024-07-25 10:45:13 -0400

    plotting

--- probability.py
+++ probability.py
@@ -12,3 +12,14 @@ sns.set_theme(rc={'figure.figsize':(5,5)})
 # import uniform distribution
 from scipy.stats import uniform
 
+# random numbers from uniform distribution
+n = 10000
+start = 10
+width = 20
+data_uniform = uniform.rvs(size=n, loc = start, scale=width)
+
+ax = sns.displot(data_uniform,
+                  bins=100,
+                  kde=True,
+                  color='skyblue')
+ax.set(xlabel='Uniform Distribution ', ylabel='Frequency')
