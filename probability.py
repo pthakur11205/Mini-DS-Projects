f7ee331f93e291b2f808476cc24b9aca110ad639
pthakur11205 <pthakur@uw.edu>
2024-07-25 10:39:10 -0400

    setting up for plotting

---
+++ probability.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+# for latex equations
+from IPython.display import Math, Latex
+# for displaying images
+from IPython.core.display import Image
+# import seaborn
+import seaborn as sns
+# settings for seaborn plotting style
+sns.set_color_codes(palette="deep")
+# settings for seaborn plot sizes
+sns.set_theme(rc={'figure.figsize':(5,5)})
+
