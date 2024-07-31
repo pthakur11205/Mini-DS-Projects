d896da21f85ac217da96d0895dc9387c85636040
pthakur11205 <pthakur@uw.edu>
2024-07-31 15:34:05 -0400

    training and building model

--- sklearning.py
+++ sklearning.py
@@ -1,6 +1,11 @@
 from sklearn.datasets import load_wine
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
 
 wine_data = load_wine() 
 wine_df = pd.DataFrame(wine_data.data, columns=wine_data.feature_names)
@@ -23,4 +28,14 @@ X_scaled = scaler.transform(X.values)
 
 # View first instance
 print(X_scaled[0])
+
+# Split data into train and test
+X_train_scaled, X_test_scaled, y_train, y_test = train_test_split(X_scaled,
+                                                                  y,
+                                                             train_size=.7,
+                                                           random_state=25)
+
+# Check the splits are correct
+print(f"Train size: {round(len(X_train_scaled) / len(X) * 100)}% \n\
+Test size: {round(len(X_test_scaled) / len(X) * 100)}%")
 
