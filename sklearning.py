c6da005b5a338066b67a6a2aaf5e006a090db4f2
pthakur11205 <pthakur@uw.edu>
2024-07-31 15:31:11 -0400

    data preprocessing

--- sklearning.py
+++ sklearning.py
@@ -1,3 +1,26 @@
 from sklearn.datasets import load_wine
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 wine_data = load_wine() 
+wine_df = pd.DataFrame(wine_data.data, columns=wine_data.feature_names)
+# Add the target label
+wine_df["target"] = wine_data.target
+
+print(wine_df.head())
+print(wine_df.describe())
+
+# Split data into features and label 
+X = wine_df[wine_data.feature_names].copy()
+y = wine_df["target"].copy() 
+
+# Instantiate scaler and fit on features
+scaler = StandardScaler()
+scaler.fit(X)
+
+# Transform features
+X_scaled = scaler.transform(X.values)
+
+# View first instance
+print(X_scaled[0])
+
