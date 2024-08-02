d5997dcb6e5aa94aeb503c1ad442f172a5d9fa5e
pthakur11205 <pthakur@uw.edu>
2024-08-02 16:18:10 -0400

    r-square

--- linreg.py
+++ linreg.py
@@ -1,2 +1,12 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
+y = np.array([5, 20, 14, 32, 22, 38])
+
+model = LinearRegression()
+model.fit(x, y)
+
+#Calculate R^2 and display
+r_sq = model.score(x, y)
+print(f"coefficient of determination: {r_sq}")
