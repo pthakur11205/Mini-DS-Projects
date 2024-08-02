46a75cf5d988ce0a2e9cae6936b218b1f77ec85d
pthakur11205 <pthakur@uw.edu>
2024-08-02 16:30:17 -0400

    data for mult regression

--- multilinreg.py
+++ multilinreg.py
@@ -1,3 +1,14 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
+x = [
+ [0, 1], [5, 1], [15, 2], 
+ [25, 5], [35, 11], [45, 15], 
+ [55, 34], [60, 35]
+ ]
+
+y = [4, 5, 20, 14, 32, 22, 38, 43]
+
+plt.plot(x, y)
+plt.show()
