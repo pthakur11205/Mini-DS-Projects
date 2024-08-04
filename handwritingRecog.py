f5611d174fc18050af3025ccfa064f03069be3f9
pthakur11205 <pthakur@uw.edu>
2024-08-04 17:49:35 -0400

    Logistic Regression in Python: Handwriting Recognition

---
+++ handwritingRecog.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+
