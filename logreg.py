f16ac1870f48c023b0319874351d3bb1b6297279
pthakur11205 <pthakur@uw.edu>
2024-08-04 17:35:01 -0400

    data and fitting

--- logreg.py
+++ logreg.py
@@ -2,3 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
+
+x = np.arange(10).reshape(-1, 1)
+y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+model = LogisticRegression(solver='liblinear', random_state=0)
+
+model.fit(x, y)
+
