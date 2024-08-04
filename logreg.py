e4a53393cf8d813b31791b17618bdea74af5b2d6
pthakur11205 <pthakur@uw.edu>
2024-08-04 17:46:01 -0400

    confusion matrix mapping

--- logreg.py
+++ logreg.py
@@ -15,3 +15,16 @@ print(model.predict_proba(x))
 print(model.predict(x))
 
 print(model.score(x, y))
+
+cm = confusion_matrix(y, model.predict(x))
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(cm)
+ax.grid(False)
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.yaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.set_ylim(1.5, -0.5)
+for i in range(2):
+    for j in range(2):
+        ax.text(j, i, cm[i, j], ha='center', va='center', color='red')
+plt.show()
