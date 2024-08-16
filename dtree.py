8f842372db24f9b9ad40dbf01364e7367932feeb
pthakur11205 <pthakur@uw.edu>
2024-08-16 13:35:42 -0400

    load dataset

--- dtree.py
+++ dtree.py
@@ -3,3 +3,9 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+# load dataset
+pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
+
+print(pima.head())
