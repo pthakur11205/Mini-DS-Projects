01d8e3dfd1de1b67de1638a8aee9bc3f785425b2
pthakur11205 <pthakur@uw.edu>
2024-08-18 14:50:26 -0400

    FRIED

--- forest.py
+++ forest.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
 import warnings
+import numpy as np
  
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import KNNImputer
@@ -10,10 +11,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
- 
+from sklearn.metrics import mean_squared_error, r2_score
+
+
 warnings.filterwarnings('ignore')
 
 df= pd.read_csv('Salaries.csv')
-print(df)
+
+# Assuming df is your DataFrame
+X = df.iloc[:,1:2].values  #features
+y = df.iloc[:,2].values  # Target variable
+
+label_encoder = LabelEncoder()
+x_categorical = df.select_dtypes(include=['object']).apply(label_encoder.fit_transform)
+x_numerical = df.select_dtypes(exclude=['object']).values
+x = pd.concat([pd.DataFrame(x_numerical), x_categorical], axis=1).values
+ 
+# Fitting Random Forest Regression to the dataset
+regressor = RandomForestRegressor(n_estimators=10, random_state=0, oob_score=True)
+ 
+# Fit the regressor with x and y data
+regressor.fit(x, y)
+
+# Access the OOB Score
+oob_score = regressor.oob_score_
+print(f'Out-of-Bag Score: {oob_score}')
+ 
+# Making predictions on the same data or new data
+predictions = regressor.predict(x)
+ 
+# Evaluating the model
+mse = mean_squared_error(y, predictions)
+print(f'Mean Squared Error: {mse}')
+ 
+r2 = r2_score(y, predictions)
+print(f'R-squared: {r2}')
+
