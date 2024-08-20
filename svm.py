325a5843ccb45202ebdd36577b849b0955557732
pthakur11205 <pthakur@uw.edu>
2024-08-20 11:35:52 -0400

    loop

--- svm.py
+++ svm.py
@@ -18,3 +18,28 @@ print(cancer.data[0:5])
 
 # print the cancer labels (0:malignant, 1:benign)
 print(cancer.target)
+
+# Import train_test_split function
+from sklearn.model_selection import train_test_split
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
+
+#Import svm model
+from sklearn import svm
+
+#Create a svm Classifier
+clf = svm.SVC(kernel='linear') # Linear Kernel
+
+#Train the model using the training sets
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+#Import scikit-learn metrics module for accuracy calculation
+from sklearn import metrics
+
+# Model Accuracy: how often is the classifier correct?
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+
