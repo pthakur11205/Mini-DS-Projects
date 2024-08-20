cea5f2e44b95fa19f62c46ebc20049613a8363c5
pthakur11205 <pthakur@uw.edu>
2024-08-20 11:34:39 -0400

    classifier

--- svm.py
+++ svm.py
@@ -3,3 +3,18 @@ from sklearn import datasets
 
 #Load dataset
 cancer = datasets.load_breast_cancer()
+
+# print the names of the 13 features
+print("Features: ", cancer.feature_names)
+
+# print the label type of cancer('malignant' 'benign')
+print("Labels: ", cancer.target_names)
+
+# print data(feature)shape
+print(cancer.data.shape)
+
+# print the cancer data features (top 5 records)
+print(cancer.data[0:5])
+
+# print the cancer labels (0:malignant, 1:benign)
+print(cancer.target)
