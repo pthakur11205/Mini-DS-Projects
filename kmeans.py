caebd5140b69b76520dc2ca8465c087a6407279c
pthakur11205 <pthakur@uw.edu>
2024-08-22 14:13:44 -0400

    elbow method

--- kmeans.py
+++ kmeans.py
@@ -4,3 +4,40 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
+
+features, true_labels = make_blobs(
+n_samples=200,
+centers=3,
+cluster_std=2.75,
+random_state=42)
+
+print(features[:5])
+
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+
+kmeans = KMeans(
+    init="random",
+    n_clusters=3,
+    n_init=10,
+    max_iter=300,
+    random_state=42
+)
+
+print(kmeans.fit(scaled_features))
+
+kmeans_kwargs = {
+     "init": "random",
+     "n_init": 10,
+     "max_iter": 300,
+     "random_state": 42,
+}
+
+ # A list holds the SSE values for each k
+sse = []
+for k in range(1, 11):
+     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+     kmeans.fit(scaled_features)
+     sse.append(kmeans.inertia_)
+
+    
