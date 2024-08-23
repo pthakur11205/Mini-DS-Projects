399b1402de8be9c5f4c8742bc9ad1dab96f98d16
pthakur11205 <pthakur@uw.edu>
2024-08-23 15:39:17 -0400

    yeah!

--- hier.py
+++ hier.py
@@ -5,5 +5,18 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 
-plt.scatter(x, y)
+data = list(zip(x, y))
+
+##For dendogram
+# linkage_data = linkage(data, method='ward', metric='euclidean')
+# dendrogram(linkage_data)
+
+# plt.show()
+
+#For color clustering
+from sklearn.cluster import AgglomerativeClustering
+hierarchical_cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')
+labels = hierarchical_cluster.fit_predict(data)
+
+plt.scatter(x, y, c=labels)
 plt.show()
