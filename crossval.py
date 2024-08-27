4022a9f14dce7ed4ed2e1a6b39c7c8158387014b
pthakur11205 <pthakur@uw.edu>
2024-08-27 11:30:29 -0400

    leave one out

--- crossval.py
+++ crossval.py
@@ -1,3 +1,33 @@
 from sklearn import datasets
 
 X, y = datasets.load_iris(return_X_y=True)
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
+
+#LOO
+from sklearn.model_selection import LeaveOneOut
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+loo = LeaveOneOut()
+
+scores = cross_val_score(clf, X, y, cv = loo)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
