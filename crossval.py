f155638bb6f1421f4724c5a74e8334b4420fffd8
pthakur11205 <pthakur@uw.edu>
2024-08-27 11:33:07 -0400

    leave-p-out

--- crossval.py
+++ crossval.py
@@ -31,3 +31,18 @@ scores = cross_val_score(clf, X, y, cv = loo)
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
+
+#LPO
+from sklearn.model_selection import LeavePOut
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+lpo = LeavePOut(p=2)
+
+scores = cross_val_score(clf, X, y, cv = lpo)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
